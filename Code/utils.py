a319377fb31a8e7cef8691c0cc895a7244bdd7c4
knsailakshmi <knsailakshmi2001@gmail.com>
2024-08-09 09:46:48 +0530

    Final Commit

--- Code/utils.py
+++ Code/utils.py
@@ -1,4 +1,6 @@
 import yaml
+from pyspark.sql import DataFrame
+import os
 
 def load_yaml_config(file_path):
     """
@@ -20,16 +22,25 @@ def read_csv_as_df(spark, file_path):
     return spark.read.option("inferSchema", "true").csv(file_path, header=True)
 
 
-def save_df_as_csv(df, file_path, format_type):
+def save_df_as_csv(df: DataFrame, path: str, format_type: str = "csv"):
     """
-    Save a Spark DataFrame to a CSV file.
-    :param df: Spark DataFrame to save
-    :param file_path: Path to the output CSV file
-    :param format_type: File format (e.g., csv)
-    :return: None
+    Save DataFrame to a CSV file.
+    :param df: DataFrame to be saved
+    :param path: Output path where the CSV will be saved
+    :param format_type: File format, e.g., "csv"
     """
-    # Uncomment the line below if you want to consolidate the output into a single file
-    # df = df.coalesce(1)
-    df.repartition(1).write.format(format_type).mode("overwrite").option(
-        "header", "true"
-    ).save(file_path)
+    # Ensure the directory exists
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    print(f"Saving DataFrame to path: {path} with format: {format_type}")
+    try:
+        df.repartition(1).write.format(format_type).mode("overwrite").option("header", "true").save(path)
+        print(f"DataFrame successfully saved to {path}")
+    except FileNotFoundError as e:
+        print(f"FileNotFoundError: {e}")
+    except PermissionError as e:
+        print(f"PermissionError: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred while saving the DataFrame: {e}")
